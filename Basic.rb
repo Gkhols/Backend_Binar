756dbcad8a232ccecf435983dbed13bca173bc59
Ghiant Masua Khols <ghiantmasuakhols@gmail.com>
2018-05-02 08:52:27 +0700

    [Ghiant] - menambahkan tugas poliandrom

--- Basic.rb
+++ Basic.rb
@@ -1,2 +1,16 @@
 #!/bin/ruby
-puts "hello world!"
+def kelulusan(nilai)
+	for i in 0..(nilai.length-1)
+		if nilai[i] <= 5
+			puts "tidak lulus"
+		elsif nilai[i] <=7
+			puts "lulus"
+		else 
+			puts "excellent"
+		end
+	end
+end
+
+nilaiku = [1,2,3,10,8,9,5,7,6]
+hasil = kelulusan(nilaiku)
+return hasil
